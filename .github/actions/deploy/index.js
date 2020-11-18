170fb9d5e68b270505473f82ebde46872a8f5c65
Andraz Vrhovec <andraz@koofr.net>
2020-11-19 00:40:20 +0100

    wip

--- .github/actions/deploy/index.js
+++ .github/actions/deploy/index.js
@@ -4,14 +4,17 @@ const core = require('@actions/core');
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-
 const environment = 'stage'
 
-// See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request("POST /repos/:owner/:repo/deployments", {
-  owner,
-  repo,
-  ref: github.context.ref,
-  environment,
-});
-console.log("Deployment created: %s", data);
+main()
+
+async function main() {
+  // See https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-a-deployment
+  const { data } = await octokit.request("POST /repos/:owner/:repo/deployments", {
+    owner,
+    repo,
+    ref: github.context.ref,
+    environment,
+  });
+  console.log("Deployment created: %s", data);
+}
