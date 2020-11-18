78fab94ef2b8e90e658debc51552d5b73c2f3b67
Andraz Vrhovec <andraz@koofr.net>
2020-11-19 00:53:39 +0100

    wip

--- .github/actions/deploy/index.js
+++ .github/actions/deploy/index.js
@@ -1,19 +1,24 @@
 const github = require('@actions/github');
+const core = require('@actions/core');
 const { request } = require("@octokit/request");
 
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-
 const environment = 'stage'
 
-main()
-
 async function main() {
-  // See https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-a-deployment
-  const { data } = await request("POST /repos/:owner/:repo/deployments", {
-    owner,
-    repo,
-    ref: github.context.ref,
-    environment,
-  });
-  console.log("Deployment created: %s", data);
+  try {
+    // See https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-a-deployment
+    const { data } = await request("POST /repos/:owner/:repo/deployments", {
+      owner,
+      repo,
+      ref: github.context.ref,
+      environment,
+    });
+    console.log("Deployment created: %s", data);
+  } catch (ex) {
+    console.log("Deployment failed: %s", ex);
+    core.setFailed(ex.message);
+  }
 }
+
+main()
