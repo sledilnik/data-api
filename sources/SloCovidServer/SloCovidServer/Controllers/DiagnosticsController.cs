2f77afc120a581edd93d7ebc1648bf000749de77
Miha Markič <miha@rthand.com>
2020-12-20 11:07:44 +0100

    Closes #65 Adds diagnostics endpoint

---
+++ sources/SloCovidServer/SloCovidServer/Controllers/DiagnosticsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SloCovidServer.Controllers
+{
+    [ApiController]
+    public class DiagnosticsController : Controller
+    {
+        [HttpHead]
+        [HttpGet]
+        [Route("diagnostics/readyz")]
+        public IActionResult Ready()
+        {
+            return Ok();
+        }
+    }
+}
