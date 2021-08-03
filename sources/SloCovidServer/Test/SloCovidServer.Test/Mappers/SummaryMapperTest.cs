bbf11f39e6defa4053ddac8fca06e6fc42a44c5b
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2021-08-03 20:54:42 +0200

    fix fix

--- sources/SloCovidServer/Test/SloCovidServer.Test/Mappers/SummaryMapperTest.cs
+++ sources/SloCovidServer/Test/SloCovidServer.Test/Mappers/SummaryMapperTest.cs
@@ -150,8 +150,8 @@ namespace SloCovidServer.Test.Mappers
         {
             StatsDaily CreateSimplifiedStats(int? confirmedToday) => new StatsDaily(default, default, default, default, default, default, default, default, default,
                 default, default, default,
-                new Cases(confirmedToday, default, default, default,default, default, default, default),
-                default, default, default, default, default, default, default, default, default);
+                new Cases(confirmedToday, default, default, default,default, default, default, default, default),
+                default, default, default, default, default, default, default, default);
             [Test]
             public void WhenNoValues_ReturnsNull()
             {
